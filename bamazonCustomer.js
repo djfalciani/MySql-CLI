4e92d8422e2045e5d67f1ea2d7bc335f34af3c4a
Danny Falciani <Daniel@danny-falcianis-macbook-pro.local>
2019-08-10 23:52:57 -0400

    moved schema and seed csv files to separate folder. Updated javascript file to include an update call to DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,17 +3,6 @@ var inquirer = require("inquirer");
 require("dotenv").config();
 const cTable = require('console.table');
 
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-
-// However, if your store does have enough of the product, you should fulfill the customer's order.
-
-// This means updating the SQL database to reflect the remaining quantity.
-// Once the update goes through, show the customer the total cost of their purchase.
-
 const connection = mysql.createConnection({
     host        : process.env.DB_HOST,
     port        : process.env.DB_PORT, 
@@ -24,51 +13,30 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    // console.log('connected as id ' + connection.threadId);
-    //start();
-    // test();
-    main();
+    mainMenu();
 });
 
-async function start() {
-   await listItems();
-   let id = await promptItemSelect();
-   console.log(id);
- }
-
- async function listItems() {
-     connection.query("select d.department_name Department, p.id Item, p.product_name Description, p.price Price, p.stock_quantity Qty from departments d, products p where d.id = p.department_id order by d.department_name, p.product_name", 
-     function(err, results) {
-        if (err) throw err;
-        console.table(results);
- });
- }
-
-
-
- // Prompt for Item selection, that will return a Promise...
-//  const promptItemSelect = () => {
-//      return inquirer.prompt([
-//          {
-//              name: "itemId",
-//              message: "What item would you like to order?"
-//          }
-//      ]);
-//  };
-
- async function promptItemSelect() {
-     return inquirer.prompt([
-         {
-             name: "itemId",
-             message: "What item would you like to order?"
-         }
-     ]);
- };
-
-async function test () {
-    let id = await promptItemSelect();
-    console.log(id);
-    return null;
+function mainMenu() {
+    inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: 'What do you want to do?',
+      choices: [
+        'Order an item',
+        'Exit application',
+      ]
+    }
+  ])
+  .then(answers => {
+    let menuId = answers.menu;
+    if (menuId === "Order an item") {
+        main();
+    } else {
+        connection.end();
+    }
+  });
 }
 
 function main() {
@@ -77,22 +45,48 @@ function main() {
   function(err, results) {
       if (err) throw err;
       console.table(results);
-    // once you have the items, prompt the user for which they'd like to bid on
-    inquirer.prompt([
-        {
-            name: "itemId",
-            message: "What item would you like to order?"
-         }
+    
+      inquirer.prompt([
+          {
+              name: "itemId",
+              type: "input",
+              message: "What item would you like to order?"
+          },
+          {
+              name: "itemQty",
+              type: "input",
+              message: "How many would you like to buy?"
+          }
     ])
     .then(function(answer) {
         // get the information of the chosen item
         var chosenItem;
-        // console.log(results);
         for (var i = 0; i < results.length; i++) {
-        if (results[i].item === answer.choice) {
-            chosenItem = results[i];
+            if (results[i].Item == answer.itemId) {
+                chosenItem = results[i];
+            }
         }
+        // See if bAmazon has enough Inventory to fulfill order...
+        let qtyOrdered = parseInt(answer.itemQty);
+        if (chosenItem.Qty >= qtyOrdered) {
+            let newQty = chosenItem.Qty - qtyOrdered;
+            orderItem(chosenItem.Item,newQty);
+
+        } else {
+            console.log("Insufficient quantity!");
+            main();
         }
+
     });
   });
+}
+
+
+function orderItem(itemId, newQty) {
+    connection.query('UPDATE products SET stock_quantity = ? WHERE id = ?', [newQty,itemId], function (error, results, fields) {
+        if (error) throw error;
+        console.log("Order placed successfully!");
+        // main();
+        mainMenu()
+    });
 }
